cbebfc2e9c2098087da617e718b613824ad1cf0c
Rick Kazman <kazman@hawaii.edu>
2026-09-09 13:16:25 -1000

    Create Ex2a.py

---
+++ Lab3/Ex2a.py
@@ -0,0 +1,8 @@
+'''
+Create a function—call it midpoint —that takes two numbers as input and returns the value halfway between them. 
+'''
+
+def midpoint(num1, num2):
+    return (num1 + num2) / 2    
+
+print (midpoint(10, 20))  # Output: 15.0
